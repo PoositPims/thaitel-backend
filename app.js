b14fb49c952d6c9ece7ea0df0f0e4fdc37ca1cff
Poosit <poosit.pims@gmail.com>
2021-10-19 10:27:01 +0700

    main

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(7777, () => console.log("server running in port 7777"));
